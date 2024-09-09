2e9bd297662760c37734c44982e7cfa9a2bde60b
seondays <agseon9@gmail.com>
2024-09-09 18:53:16 +0900

    chore: init

--- src/main/java/com/example/securityStudy/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securityStudy.repository;
-
-import com.example.securityStudy.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-}
